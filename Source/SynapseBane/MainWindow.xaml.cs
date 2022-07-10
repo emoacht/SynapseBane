898956ca6cc6f1f09c03ef805d933eacefa47497
emoacht <emotom@pobox.com>
2022-07-10 09:12:32 +0900

    Redesign

--- Source/SynapseBane/MainWindow.xaml.cs
+++ Source/SynapseBane/MainWindow.xaml.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Text;
@@ -21,5 +20,26 @@ namespace SynapseBane
 		{
 			InitializeComponent();
 		}
+
+		public override void OnApplyTemplate()
+		{
+			base.OnApplyTemplate();
+
+			this.TitleBar.MouseDown += (_, e) =>
+			{
+				if (Mouse.LeftButton == MouseButtonState.Pressed)
+					this.DragMove();
+			};
+			this.TitleBar.MouseUp += (_, _) => RevertAeroSnap();
+			this.TitleBar.MouseLeave += (_, _) => RevertAeroSnap();
+
+			void RevertAeroSnap()
+			{
+				if (WindowState is WindowState.Maximized)
+					WindowState = WindowState.Normal;
+			}
+		}
+
+		private void CloseButton_Click(object sender, RoutedEventArgs e) => this.Close();
 	}
 }
